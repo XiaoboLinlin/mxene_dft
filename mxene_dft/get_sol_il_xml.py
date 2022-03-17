98a002715d32c3f53a80f3cb13317b0afa6a09df
XiaoboLinlin <xiaobolin11@outlook.com>
2022-03-17 14:24:57 -0700

    update_ff

--- mxene_dft/get_sol_il_xml.py
+++ mxene_dft/get_sol_il_xml.py
@@ -1,6 +1,7 @@
 import inspect
 import os
 import mbuild as mb
+import pathlib
 from pkg_resources import resource_filename
 
 # def GetSolv(solv_name):
@@ -28,8 +29,12 @@ from pkg_resources import resource_filename
 #     ff_path = os.path.join(cache_dir, ff_name + '.xml')
 #     return ff_path
 
+import os
+path = os.path.abspath(__file__)
+dir_path = os.path.dirname(path)
+
 def GetSolv(solv_name):
-    cache_dir = '/raid6/homes/linx6/forcefield/mol2'
+    cache_dir = '{}/forcefield/mol2'.format(dir_path)
     filename = '{}.mol2'.format(solv_name)
     if any(file == filename for file in os.listdir(cache_dir)):
         solv = mb.load(os.path.join(cache_dir, filename))
@@ -37,7 +42,7 @@ def GetSolv(solv_name):
     return solv
 
 def GetIL(il_name):
-    cache_dir = '/raid6/homes/linx6/forcefield/mol2'
+    cache_dir = '{}/forcefield/mol2'.format(dir_path)
     filename = '{}.mol2'.format(il_name)
     if any(file == filename for file in os.listdir(cache_dir)):
         il = mb.load(os.path.join(cache_dir, filename))
@@ -49,6 +54,6 @@ def GetIL(il_name):
 def Get_ff_path(ff_name):
     """Get the path to a force field xml file """
     """in a directory of the same name."""
-    cache_dir = '/raid6/homes/linx6/forcefield'
+    cache_dir = '{}/forcefield'.format(dir_path)
     ff_path = os.path.join(cache_dir, ff_name + '.xml')
     return ff_path
